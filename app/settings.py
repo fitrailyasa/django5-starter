c4ab54c73d0cb17b65bad78d7586343d72902bed
Fitra Ilyasa <85192292+fitrailyasa@users.noreply.github.com>
2024-01-29 01:28:40 +0700

    update unix socket

--- app/settings.py
+++ app/settings.py
@@ -13,9 +13,9 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 import os
 from dotenv import load_dotenv
 from pathlib import Path
+from decouple import config
 
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
